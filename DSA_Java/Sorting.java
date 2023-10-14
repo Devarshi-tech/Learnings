49b731c53dcfedfdc558aaf378a142517c82c2f7
Devarshi-tech <devarshi330@gmail.com>
2023-10-14 17:59:51 +0530

    updates

--- DSA_Java/Sorting.java
+++ DSA_Java/Sorting.java
@@ -4,17 +4,17 @@ class Sorting {
         int[] arr = {7,8,3,1,2};
 
         //--------------- bubble sort -------------------
-        // for(int i=0;i<(arr.length-1);i++){
-        //     for(int j=0;j<(arr.length-i-1);j++){
-        //         if(arr[j] > arr[j+1]){
-        //             // swap values
-        //             int tempVal = arr[j];
-        //             arr[j] = arr[j+1];
-        //             arr[j+1] = tempVal;
-        //         }
-        //     }
-        // }
-        // System.out.println(Arrays.toString(arr));
+        for(int i=0;i<(arr.length-1);i++){
+            for(int j=0;j<(arr.length-i-1);j++){
+                if(arr[j] > arr[j+1]){
+                    // swap values
+                    int tempVal = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = tempVal;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
         
         //time complexity = O(n^2)
         //---------------- Selection sort ---------------------
